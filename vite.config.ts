105644ba05cce020c996331b95ebd299129351cc
Matthew Ross <mer148@pitt.edu>
2025-04-21 13:37:37 +0200

    Updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/campaign-design/',
+  base: 'https://maedross.github.io/campaign-design/',
   plugins: [
     vue(),
     vueDevTools(),
