2254fde26dd90ba6ffccb48d1fbb595b75f3b3c0
Matthew Ross <mer148@pitt.edu>
2025-05-06 10:12:00 +0200

    Updated history mode

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHashHistory, createRouter } from 'vue-router'
 
 import GamesListView from './views/GamesListView.vue'
 import ModelView from './views/ModelView.vue'
@@ -12,7 +12,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
